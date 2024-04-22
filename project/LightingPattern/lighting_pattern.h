92ea83cfd94024d8b9871b38509d1b3a4e146ee8
box16 <inoinobox16@gmail.com>
2024-04-22 19:42:52 +0900

    リファクタ

--- project/LightingPattern/lighting_pattern.h
+++ project/LightingPattern/lighting_pattern.h
@@ -14,11 +14,11 @@ public:
         {
             uint x = (i + (time >> 1)) % 64;
             if (x < 10)
-                pixel_interface.light_up(ColorCode(0xff, 0, 0));
+                pixel_interface.light_up(ColorCode(255, 0, 0));
             else if (x >= 15 && x < 25)
-                pixel_interface.light_up(ColorCode(0, 0xff, 0));
+                pixel_interface.light_up(ColorCode(0, 255, 0));
             else if (x >= 30 && x < 40)
-                pixel_interface.light_up(ColorCode(0, 0, 0xff));
+                pixel_interface.light_up(ColorCode(0, 0, 255));
             else
                 pixel_interface.light_up(ColorCode(0, 0, 0));
         }
@@ -32,14 +32,14 @@ public:
                   const uint pixel_num,
                   const uint time) const
     {
-        double phase = (time % 360) * (M_PI / 180.0);
-        uint8_t red_intensity = static_cast<uint8_t>((sin(phase) * 0.5 + 0.5) * 255);
-        uint8_t green_intensity = static_cast<uint8_t>((sin(phase + 2 * M_PI / 2) * 0.5 + 0.5) * 255);
-        uint8_t blue_intensity = static_cast<uint8_t>((sin(phase + 3 * M_PI / 2) * 0.5 + 0.5) * 255);
+        const float radian = (time % 360) * (M_PI / 180.0);
+        const uint8_t red = static_cast<uint8_t>((sin(radian) * 0.5 + 0.5) * 255);
+        const uint8_t green = static_cast<uint8_t>((sin(radian + 2 * M_PI / 2) * 0.5 + 0.5) * 255);
+        const uint8_t blue = static_cast<uint8_t>((sin(radian + 3 * M_PI / 2) * 0.5 + 0.5) * 255);
 
         for (uint i = 0; i < pixel_num; i++)
         {
-            pixel_interface.light_up(ColorCode(red_intensity, green_intensity, blue_intensity));
+            pixel_interface.light_up(ColorCode(red, green, blue));
         }
     }
 };
