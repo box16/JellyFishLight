cdcf27da1dea8ecc66299f06cba7d32934da79a2
box16 <inoinobox16@gmail.com>
2024-04-22 20:48:21 +0900

    ボタン入力実装

--- project/main.cpp
+++ project/main.cpp
@@ -9,14 +9,21 @@
 #include "hardware/pio.h"
 #include "Pixel/pixel.h"
 #include "LightingPattern/lighting_pattern.h"
+#include <memory>
+#include <array>
 
 static constexpr bool IS_RGBW = false;
 static constexpr uint8_t NUM_PIXELS = 5;
 static constexpr uint8_t WS2812_PIN = 28;
+static constexpr uint8_t SWITCH = 27;
+static constexpr uint8_t PATTERN_NUM = 3;
 
 int main()
 {
     stdio_init_all();
+    gpio_init(SWITCH);
+    gpio_set_dir(SWITCH, GPIO_IN);
+    gpio_pull_down(SWITCH);
 
     PIO pio = pio0;
     int sm = 0;
@@ -24,13 +31,32 @@ int main()
 
     ws2812_program_init(pio, sm, offset, WS2812_PIN, 800000, IS_RGBW);
 
-    RandomFadePattern pattern;
+    std::array<std::unique_ptr<LightPatternInterface>, PATTERN_NUM> patterns = {
+        std::make_unique<SimpleAquaMarinePattern>(),
+        std::make_unique<FadePattern>(),
+        std::make_unique<RandomFadePattern>()};
     Pixel pixel(pio);
     uint counter = 0;
+
+    uint switch_counter = 0;
+    uint64_t last_debounce_time = 0;
+    const uint64_t debounce_delay = 200;
+
     while (1)
     {
-        pattern.light_up(pixel, NUM_PIXELS, counter);
+        bool button_pressed = gpio_get(SWITCH) == 1;
+        if (button_pressed)
+        {
+            uint64_t current_time = to_us_since_boot(get_absolute_time());
+            if (current_time - last_debounce_time > debounce_delay * 1000)
+            {
+                switch_counter++;
+                last_debounce_time = current_time;
+                counter = 0;
+            }
+        }
+        patterns[switch_counter % PATTERN_NUM]->light_up(pixel, NUM_PIXELS, counter);
         counter++;
-        sleep_ms(10);
+        sleep_ms(50);
     }
 }
