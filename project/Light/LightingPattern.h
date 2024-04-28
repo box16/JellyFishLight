1ca475fb2e5b39b948c34b3b6d2648d01db75699
box16 <inoinobox16@gmail.com>
2024-04-28 18:32:44 +0900

    RGBのvector生成お試し

--- project/Light/LightingPattern.h
+++ project/Light/LightingPattern.h
@@ -7,23 +7,26 @@
 class SimpleAquaMarinePattern : public LightingPatternInterface
 {
 public:
-    void LightUp(const LEDInterface &led_interface,
-                 const uint pixel_num,
-                 const uint time) const
+    std::vector<RGB> LightUp(const LEDInterface &led_interface,
+                             const uint pixel_num,
+                             const uint time) const
     {
+        std::vector<RGB> result(pixel_num);
         for (uint i = 0; i < pixel_num; i++)
         {
-            led_interface.LightUp(RGB(0x7f, 0xff, 0xd4));
+            result[i] = RGB(0x7f, 0xff, 0xd4);
+            led_interface.LightUp(result[i]);
         }
+        return result;
     }
 };
 
 class RandomFadePattern : public LightingPatternInterface
 {
 public:
-    void LightUp(const LEDInterface &led_interface,
-                 const uint pixel_num,
-                 const uint time) const
+    std::vector<RGB> LightUp(const LEDInterface &led_interface,
+                             const uint pixel_num,
+                             const uint time) const
     {
         const float radian = (time % 360) * (M_PI / 180.0);
         const uint8_t color1 = static_cast<uint8_t>((sin(radian) * 0.5 + 0.5) * 255);
@@ -33,30 +36,36 @@ public:
         std::random_device rd;
         std::mt19937 g(rd());
 
+        std::vector<RGB> result(pixel_num);
         for (uint i = 0; i < pixel_num; i++)
         {
             uint8_t colors[3] = {color1, color2, color3};
             std::shuffle(std::begin(colors), std::end(colors), g);
-            led_interface.LightUp(RGB(colors[0], colors[1], colors[2]));
+            result[i] = RGB(colors[0], colors[1], colors[2]);
+            led_interface.LightUp(result[i]);
         }
+        return result;
     }
 };
 
 class FadePattern : public LightingPatternInterface
 {
 public:
-    void LightUp(const LEDInterface &led_interface,
-                 const uint pixel_num,
-                 const uint time) const
+    std::vector<RGB> LightUp(const LEDInterface &led_interface,
+                             const uint pixel_num,
+                             const uint time) const
     {
         const float radian = (time % 360) * (M_PI / 180.0);
         const uint8_t red = static_cast<uint8_t>((sin(radian) * 0.5 + 0.5) * 255);
         const uint8_t green = static_cast<uint8_t>((sin(radian + 2 * M_PI / 2) * 0.5 + 0.5) * 255);
         const uint8_t blue = static_cast<uint8_t>((sin(radian + 3 * M_PI / 2) * 0.5 + 0.5) * 255);
 
+        std::vector<RGB> result(pixel_num);
         for (uint i = 0; i < pixel_num; i++)
         {
-            led_interface.LightUp(RGB(red, green, blue));
+            result[i] = RGB(red, green, blue);
+            led_interface.LightUp(result[i]);
         }
+        return result;
     }
 };
--- project/Light/LightingPatternInterface.h
+++ project/Light/LightingPatternInterface.h
@@ -1,10 +1,11 @@
 #pragma once
 #include "LEDInterface.h"
-
+#include "RGB.h"
+#include <vector>
 class LightingPatternInterface
 {
 public:
-    virtual void LightUp(const LEDInterface &led_interface,
-                         const uint len,
-                         const uint time) const = 0;
+    virtual std::vector<RGB> LightUp(const LEDInterface &led_interface,
+                                     const uint len,
+                                     const uint time) const = 0;
 };
